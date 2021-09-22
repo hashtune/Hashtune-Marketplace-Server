3aa46fa64828a92432c5ad8a1a45bb179e724429
thatsddr <domenicodiruocco01@gmail.com>
2021-09-22 12:47:08 +0200

    tests: separated test files

--- __tests__/queries/ArtworkQueryTest.spec.ts
+++ __tests__/queries/ArtworkQueryTest.spec.ts
@@ -9,14 +9,6 @@ describe('Test users query', () => {
     }
 `;
 
-  const USER_QUERY = `
-    query Query {
-      listTrendyCreators {
-        handle
-      }
-    }
-`;
-
   it('should query only auctions ', async () => {
     const res = await server.executeOperation({
       query: ARTWORK_QUERY,
@@ -41,14 +33,7 @@ describe('Test users query', () => {
     });
     expect(res).toMatchSnapshot();
   });
-
-  // TODO actually return data
-  it('should query the trending users', async () => {
-    const res = await server.executeOperation({
-      query: USER_QUERY,
-    });
-    expect(res).toMatchSnapshot();
-  });
 });
 
-export {};
+export { };
+
