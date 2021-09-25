cd16c875b3d7a7ea1907014a045d8f0d6fe28570
eemmiillyy <emily.morgan@code.berlin>
2021-09-25 12:46:54 +0200

    See if teardown process exit makes a diff

--- __tests__/teardown.ts
+++ __tests__/teardown.ts
@@ -5,6 +5,7 @@ export default async function teardown(): Promise<void> {
   try {
     await reset();
     server.stop();
+    process.exit(1);
   } catch (e) {
     throw new Error('Issue tearing down test');
   }
