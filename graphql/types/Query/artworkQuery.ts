387f6d2e7ca63970ac0279e3a06ccf3e3087fdd3
thatsddr <domenicodiruocco01@gmail.com>
2021-09-25 15:09:43 +0200

    fix: each query now has its own file

---
+++ graphql/types/Query/artworkQuery.ts
@@ -0,0 +1,28 @@
+import { booleanArg, extendType } from 'nexus';
+import { Context } from '../../context';
+
+export const ArtworkQuery = extendType({
+    type: 'Query',
+    definition(t) {
+        t.list.field('listArtworks', {
+            type: 'Artwork',
+            description:
+                'If only auction argument is true then all auctions are returned. If not then all artworks are returned. ',
+            args: {
+                auction: booleanArg(),
+            },
+            resolve: async (_, args, ctx: Context) => {
+                if (args.auction) {
+                    return ctx.prisma.artwork.findMany({
+                        where: {
+                            saleType: 'auction',
+                        },
+                    });
+                }
+                // Both sale and non sale
+                return ctx.prisma.artwork.findMany({});
+            },
+        });
+
+    },
+});
