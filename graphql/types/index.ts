9eb0780acd89fd36b65120e0dc86e954f8030eaa
thatsddr <domenicodiruocco01@gmail.com>
2021-09-25 16:47:44 +0200

    import mutation

--- graphql/types/index.ts
+++ graphql/types/index.ts
@@ -2,5 +2,6 @@ export * from './Artwork';
 export * from './Auction';
 export * from './Bid';
 export * from './Errors';
+export * from './Mutation';
 export * from './Query';
 export * from './User';
