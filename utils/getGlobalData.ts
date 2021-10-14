69e6c8114e265360bb1a3b3c8ea6aee8c21264a2
thatsddr <domenicodiruocco01@gmail.com>
2021-10-14 17:25:54 +0200

    fix: added wallets to test global data

--- utils/getGlobalData.ts
+++ utils/getGlobalData.ts
@@ -3,6 +3,7 @@ import { prisma } from '../singletons/prisma';
 export default async function getGlobalData() {
     const users = await prisma.user.findMany({});
     const artworks = await prisma.artwork.findMany({});
-    if (!users || !artworks) throw new Error("Unable to set up global data");
-    return { users, artworks }
+    const wallets = await prisma.wallet.findMany({});
+    if (!users || !artworks || !wallets) throw new Error("Unable to set up global data");
+    return { users, artworks, wallets }
 }
