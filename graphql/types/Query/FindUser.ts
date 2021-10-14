c669838fb5fa3daa88d80539c705780906abc74c
thatsddr <domenicodiruocco01@gmail.com>
2021-10-14 17:03:11 +0200

    feat: added possibility to find user by public Key

--- graphql/types/Query/FindUser.ts
+++ graphql/types/Query/FindUser.ts
@@ -1,4 +1,4 @@
-import { extendType, nonNull, stringArg } from 'nexus';
+import { extendType, stringArg } from 'nexus';
 import { Context } from '../../context';
 
 export const FindUser = extendType({
@@ -6,16 +6,35 @@ export const FindUser = extendType({
   definition(t) {
     t.field('findUser', {
       type: 'UserResult',
-      description: 'Find an user by handle',
-      args: { handle: nonNull(stringArg()) },
+      description: 'Find an user by handle or public key',
+      args: { handle: stringArg(), publicKey: stringArg() },
       resolve: async (_, args, ctx: Context) => {
-        const res = await ctx.prisma.user.findUnique({
-          where: { handle: args.handle },
-        });
+
+        let res;
+        let errorMessage: string;
+
+        if (args.handle && !args.publicKey) {
+          res = await ctx.prisma.user.findUnique({
+            where: { handle: args.handle },
+          });
+          errorMessage = `Couldn't find user with handle ${args.handle}`
+        } else if (args.publicKey) {
+          const wallet = await ctx.prisma.wallet.findUnique({ where: { publicKey: args.publicKey } })
+          res = wallet && await ctx.prisma.user.findUnique({
+            where: {
+              walletId: wallet.id
+            }
+          });
+          errorMessage = `Couldn't find user with publicKey ${args.publicKey}`
+        } else {
+          res = null;
+          errorMessage = `Please specify one of the optional fields.`
+        }
+
         if (res) {
           return { Users: [res] };
         } else {
-          return { ClientErrorUserNotFound: { message: `Couldn't find user with handle ${args.handle}` } }
+          return { ClientErrorUserNotFound: { message: errorMessage } }
         }
       },
     });
