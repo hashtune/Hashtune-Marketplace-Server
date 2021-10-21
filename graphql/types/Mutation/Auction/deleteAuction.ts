da787367477b9889487e3cc0b44817d827f64781
thatsddr <domenicodiruocco01@gmail.com>
2021-10-21 15:21:42 +0200

    feat. added mutation to delete auctions

---
+++ graphql/types/Mutation/Auction/deleteAuction.ts
@@ -0,0 +1,29 @@
+import { extendType, nonNull, stringArg } from 'nexus';
+import { Context } from '../../../context';
+
+export const deleteAuction = extendType({
+    type: "Mutation",
+    definition(t) {
+        t.field("deleteAuction", {
+            type: "AuctionResult",
+            description: "delete an Auction if it has no bids",
+            args: {
+                userId: nonNull(stringArg()),
+                auctionId: nonNull(stringArg())
+            },
+            resolve: async (_, args, ctx: Context) => {
+                const auctionData = await ctx.prisma.auction.findUnique({ where: { id: args.auctionId } });
+                if (!auctionData) return { ClientErrorAuctionNotFound: { message: "Couldn't find an auction with the specified id" } }
+                if (auctionData.currentHighBidder) return { CLientErrorAuctionNotDeletable: { message: "Cannot delete an auction with bids" } }
+
+                const artworkData = await ctx.prisma.artwork.findUnique({ where: { id: auctionData.artworkId } })
+                if (!artworkData) return { ClientErrorArtworkNotFound: { message: `Couldn't find an artwork with id ${args.artworkId}` } }
+                if (artworkData.ownerId !== args.userId) return { ClientErrorUserUnauthorized: { message: `The user is not the owner of the artwork` } }
+
+                const res = await ctx.prisma.auction.delete({ where: { id: args.auctionId } })
+                return res ? ({ "Auctions": [res] }) : ({ ClientErrorUnknown: { message: "Error while deleting the auction" } })
+            }
+        })
+    }
+
+})
