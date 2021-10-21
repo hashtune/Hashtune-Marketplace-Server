eea2feba10ce4c34f752b41fd0705a1f8124cbed
thatsddr <domenicodiruocco01@gmail.com>
2021-10-21 14:25:41 +0200

    test: added tests for auction mutation

---
+++ __tests__/mutations/AuctionMutation.spec.ts
@@ -0,0 +1,76 @@
+import getGlobalData from '../../utils/getGlobalData';
+import reset from '../../utils/reset';
+import seed from '../../utils/seed';
+import server from '../server';
+
+describe("test auction mutation", () => {
+
+    let users;
+    let artworks;
+
+    beforeAll(async () => {
+        await reset();
+        await seed();
+        global.testData = await getGlobalData();
+        users = global.testData.users;
+        artworks = global.testData.artworks;
+    });
+
+    const ADD_AUCTION_MUTATION = `
+        mutation addAuction2($inputType: AddAuctionInput) {
+            addAuction(InputType: $inputType) {
+                Auctions {
+                    currentHigh
+                }
+                ClientErrorArtworkNotFound {
+                    message
+                }
+                ClientErrorUserUnauthorized {
+                    message
+                }
+                ClientErrorUnknown {
+                    message
+                }
+                ClientErrorArtworkAlreadyExists {
+                    message
+                }
+                ClientErrorArtworkNotAnAuction {
+                    message
+                }
+            }
+        }
+    `
+
+
+    const createAuction = async (userId, artworkId) => {
+        return await server.executeOperation({
+            query: ADD_AUCTION_MUTATION, variables: {
+                "inputType": {
+                    "userId": userId,
+                    "artworkId": artworkId,
+                    "live": true
+                }
+            }
+        })
+    }
+
+    it("Should create a new auction", async () => {
+        const res = await createAuction(users[1].id, artworks[0].id);
+        expect(res).toMatchSnapshot()
+    })
+
+    it("Shouldn't let an unauthorized user create a new auction", async () => {
+        const res = await createAuction(users[1].id, artworks[1].id);
+        expect(res).toMatchSnapshot()
+    })
+
+    it("should fail because it cannot find the id", async () => {
+        const res = await createAuction(users[1].id, "noid");
+        expect(res).toMatchSnapshot()
+    })
+
+    it("Should not create a new auction for this artwork because one already exists", async () => {
+        const res = await createAuction(users[1].id, artworks[0].id);
+        expect(res).toMatchSnapshot()
+    })
+})
