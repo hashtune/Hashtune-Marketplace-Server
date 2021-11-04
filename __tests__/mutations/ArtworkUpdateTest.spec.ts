a5f523b61a77bd6969455f5b2d2bd2676be78f78
thatsddr <domenicodiruocco01@gmail.com>
2021-11-04 16:41:43 +0100

    fix: remove test to check that the user is the owner of the artwork

--- __tests__/mutations/ArtworkUpdateTest.spec.ts
+++ __tests__/mutations/ArtworkUpdateTest.spec.ts
@@ -5,22 +5,18 @@ import seed from '../../utils/seed';
 import server from '../server';
 
 describe('Test artwork update mutation', () => {
-    chain.checkSuccessLog = jest.fn();
+  chain.checkSuccessLog = jest.fn();
 
-    let users;
-    let artworks;
-    let auctions;
+  let artworks;
 
-    beforeAll(async () => {
-        await reset();
-        await seed();
-        global.testData = await getGlobalData();
-        users = await global.testData.users;
-        artworks = await global.testData.artworks;
-        auctions = await global.testData.auctions;
-    });
+  beforeAll(async () => {
+    await reset();
+    await seed();
+    global.testData = await getGlobalData();
+    artworks = await global.testData.artworks;
+  });
 
-    const UPDATE_ARTWORK_MUTATION = `
+  const UPDATE_ARTWORK_MUTATION = `
     mutation UpdateArtwork($inputType: UpdateArtworkInput) {
         updateArtwork(InputType: $inputType) {
           Artworks {
@@ -51,37 +47,30 @@ describe('Test artwork update mutation', () => {
       }
     `
 
-    const updateAuction = async (artworkId: string, userId: string, salePrice: undefined | number = undefined, reservePrice: undefined | number = undefined) => {
-        return await server.executeOperation({
-            query: UPDATE_ARTWORK_MUTATION, variables: {
-                inputType: {
-                    txHash: "000000",
-                    artworkId,
-                    userId,
-                    salePrice,
-                    reservePrice
-                }
-            }
-        })
-    }
-
-    it("Should update the reservePrice", async () => {
-        const res = await updateAuction(artworks[0].id, users[1].id, undefined, 13)
-        expect(res).toMatchSnapshot()
+  const updateAuction = async (artworkId: string, salePrice: undefined | number = undefined, reservePrice: undefined | number = undefined) => {
+    return await server.executeOperation({
+      query: UPDATE_ARTWORK_MUTATION, variables: {
+        inputType: {
+          artworkId,
+          salePrice,
+          reservePrice
+        }
+      }
     })
+  }
 
-    it("Should throw an error because price is not specified for a fixed sale", async () => {
-        const res = await updateAuction(artworks[4].id, users[1].id, undefined, undefined)
-        expect(res).toMatchSnapshot()
-    })
+  it("Should update the reservePrice", async () => {
+    const res = await updateAuction(artworks[0].id, undefined, 13)
+    expect(res).toMatchSnapshot()
+  })
 
-    it("Should update the price", async () => {
-        const res = await updateAuction(artworks[4].id, users[0].id, 80, undefined)
-        expect(res).toMatchSnapshot()
-    })
+  it("Should throw an error because price is not specified for a fixed sale", async () => {
+    const res = await updateAuction(artworks[4].id, undefined, undefined)
+    expect(res).toMatchSnapshot()
+  })
 
-    it("Should throw an error because the user is not the owner of the Artwork", async () => {
-        const res = await updateAuction(artworks[4].id, users[2].id, 10, undefined)
-        expect(res).toMatchSnapshot()
-    })
+  it("Should update the price", async () => {
+    const res = await updateAuction(artworks[4].id, 80, undefined)
+    expect(res).toMatchSnapshot()
+  })
 })
