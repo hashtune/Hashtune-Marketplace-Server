880f0ad503618ce56be53c3f5c594790d700bc40
thatsddr <domenicodiruocco01@gmail.com>
2021-11-18 11:15:43 +0100

    feat: add createBid event

--- constants.ts
+++ constants.ts
@@ -1,4 +1,6 @@
-export const createEvent =
+export const createTokenEvent =
   'event TokenCreated(address by,uint256 tokenId,address[] creators,uint256[] creatorsRoyalty,uint8 status,bytes32 digest,uint8 hashFunction,uint8 size)';
+
+export const createBidEvent = 'event NewBid(address by, uint256 tokenId, uint256 amount);'
 
 export const contractAddress = '0xAf266B3D45B11D8B3f28dc2427745c3970B0368C';
